20e9d6148989bcca2da5a8184b3acce345f508c3
Emma <emma.polanco@enti.cat>
2023-10-06 20:03:34 +0200

    Cohesion, working on it, just made myself a base

---
+++ SDL_Steering_Behaviors/SDL_Steering_Behaviors/CohesionBehavior.cpp
@@ -0,0 +1,20 @@
+#include "CohesionBehavior.h"
+
+#include "CohesionBehavior.h"
+
+CohesionBehavior::CohesionBehavior()
+{
+    // Constructor implementation if needed
+}
+
+Vector2D CohesionBehavior::CalculateForces(Agent* agent, Vector2D target, float dtime)
+{
+    // Calculate the cohesion force based on agent and target
+    // Return the calculated force as a Vector2D
+}
+
+Vector2D CohesionBehavior::CalculateForces(Agent* agent, Agent* target, float dtime)
+{
+    // Calculate the cohesion force based on two agents
+    // Return the calculated force as a Vector2D
+}
---
+++ SDL_Steering_Behaviors/SDL_Steering_Behaviors/CohesionBehavior.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "SteeringBehavior.h"
+#include <vector>
+
+class Agent;
+
+class CohesionBehavior : public SteeringBehavior
+{
+public:
+    CohesionBehavior();
+    virtual Vector2D CalculateForces(Agent* agent, Vector2D target, float dtime) override;
+    virtual Vector2D CalculateForces(Agent* agent, Agent* target, float dtime) override;
+};
+
